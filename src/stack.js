3e3eba07d9caf14f7e4764cb3fe37bc7822c095e
xWolfYx <david.skliarov.dev@gmail.com>
2025-11-10 13:50:44 +0400

    feat: implement stack.js

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib/errors');
+const { NotImplementedError } = require("../lib/errors");
 
 /**
  * Implement the Stack with a given interface via array.
@@ -13,19 +13,21 @@ const { NotImplementedError } = require('../lib/errors');
  *
  */
 class Stack {
-  push(/* value */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  constructor() {
+    this.items = [];
+  }
+  push(value) {
+    return this.items.push(value);
   }
 
   pop() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    return this.items.length === 0 ? undefined : this.items.pop();
   }
 
   peek() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    return this.items.length === 0
+      ? undefined
+      : this.items[this.items.length - 1];
   }
 }
 
